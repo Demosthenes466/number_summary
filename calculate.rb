7dabaa178d9026e8de65ba2b6eef1eb6d7d77be3
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2015-09-28 10:22:04 -0600

    We made things

--- calculate.rb
+++ calculate.rb
@@ -5,12 +5,15 @@ a = (1..6).map { |i| i }
 file = File.open("data.csv", 'r')
 		file.each_line do |line|
 			num_array = line.split(",")
-			print num_array 
+			# print num_array 
 			puts
-		
+
 print NumberSummary.min(num_array), "\n"
 print NumberSummary.max(num_array), "\n"
 print NumberSummary.mean(num_array), "\n"
 print NumberSummary.median(num_array), "\n"
+print NumberSummary.q_one(num_array), "\n"
+print NumberSummary.q_three(num_array), "\n"
+print NumberSummary.mode(num_array), "\n"
 end
 # puts NumberSummary.summarize('data.csv')
--- number_summary.rb
+++ number_summary.rb
@@ -31,6 +31,7 @@ class NumberSummary
 				end
 
 				final = total/array.length
+				final = final.round(2)
 				puts "Mean Value is: #{final}"
 			end
 
@@ -41,16 +42,95 @@ class NumberSummary
 					median_array[i] = array[i].to_f
 				end
 				sorted = median_array.sort
-				if ((sorted.length)% 2 == 0)
-					median = (sorted[(sorted.length/2)].to_f + sorted[(sorted.length/2)-1].to_f).to_f/2
-				else 
-					median = sorted[(sorted.length/2).to_i]
+					if ((sorted.length)% 2 == 0)
+						median = (sorted[(sorted.length/2)].to_f + sorted[(sorted.length/2)-1].to_f).to_f/2
+					else 
+						median = sorted[(sorted.length/2).to_i]
+					end
+					median = median.round(1)
+					puts "Median value is: #{median}"
 				end
-				puts "Median value is: #{median}"
-				
-			
+
+			def q_one(array)
+				median_array = Array.new
+				for i in (0..array.length - 1) do
+					median_array[i] = array[i].to_f
+				end
+				sorted = median_array.sort
+					if ((sorted.length)% 2 == 0)
+						q1 = Array.new
+						q1 = sorted[0..(sorted.length/2)-1]
+						if ((q1.length)% 2 == 0)
+							median = (q1[(q1.length/2)].to_f + q1[(q1.length/2)-1].to_f).to_f/2
+						else 
+							median = q1[(q1.length/2).to_i]
+						end
+					else 
+						q1 = Array.new
+						q1 = sorted[0..(sorted.length/2)-1]
+					if ((q1.length)% 2 == 0)
+							median = (q1[(q1.length/2)].to_f + q1[(q1.length/2)-1].to_f).to_f/2
+						else 
+							median = q1[(q1.length/2).to_i]
+						end
+					end
+					median = median.round(1)
+					puts "Quartile 1 is:" "#{median}"
+			end
+
+			def q_three(array)
+				median_array = Array.new
+				for i in (0..array.length - 1) do
+					median_array[i] = array[i].to_f
+				end
+				sorted = median_array.sort
+					if ((sorted.length)% 2 == 0)
+						q1 = Array.new
+						q1 = sorted[((sorted.length/2)+1)..sorted.length]
+						if ((q1.length)% 2 == 0)
+							median = (q1[(q1.length/2)].to_f + q1[(q1.length/2)-1].to_f).to_f/2
+						else 
+							median = q1[(q1.length/2).to_i]
+						end
+					else 
+						q1 = Array.new
+						q1 = sorted[((sorted.length/2)+1)..sorted.length]
+					if ((q1.length)% 2 == 0)
+							median = (q1[(q1.length/2)].to_f + q1[(q1.length/2)-1].to_f).to_f/2
+						else 
+							median = q1[(q1.length/2).to_i]
+						end
+					end
+					median = median.round(1)
+					puts "Quartile 3 is:" "#{median}"
+			end
+
+			def mode(array)
+				n = 0
+				median_array = Array.new
+				for i in (0..array.length - 1) do
+					median_array[i] = array[i].to_f
+				end
+				sorted = median_array.sort
+				print sorted
+				mode_array = Array.new
+				index.to_f = 0
+				for i in (0..sorted.length)
+					index = sorted[i].index
+					print index
+					if((sorted[index] == sorted[index-1]))
+						mode_array.push(sorted[index])
+						print "running"
+					end
+					# if(i == n)
+					# 	mode_array.push(n)
+					# 	print n
+					# 	print "running"
+					# end
+					# n = i
+				end
+				print mode_array
+
 			end
 	end
-			
-
 end
