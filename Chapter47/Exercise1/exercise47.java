c11fb2527891fee56eb0bad37f93a642570191c8
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2016-01-13 10:48:42 -0700

    Initial commit

---
+++ Chapter47/Exercise1/exercise47.java
@@ -0,0 +1,36 @@
+import java.io.* ;
+
+class ThreeSums
+{
+	public static void main (String[] args)
+	{
+		int[] data = {3,2,5,7,9,12,97,24,54};
+
+		//declare and initialize three sums
+		int totalSum = 0;
+		int evenSum = 0;
+		int oddSum = 0;
+		//compute the sums
+		for (int index=0; index < data.length; index++) {
+			totalSum += data[index];
+			if (data[index] % 2 == 0) {
+				evenSum += data[index];
+	  	} else {
+	  		oddSum += data[index];
+	  	}
+
+
+		}
+
+		// write out the three sums
+		System.out.println("Total Sum: " + totalSum);
+		System.out.println("Even Sum: " + evenSum);
+		System.out.println("Odd Sum: " + oddSum);
+
+
+	}
+
+
+
+
+}
