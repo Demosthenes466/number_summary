e313cfdd84ad7a0f1a5c21d0983a92b471f520cb
Demosthenes466 <ziehmtj@s.dcsdk12.org>
2016-01-17 21:59:34 -0700

    I fixed exercise 3

--- Chapter47/exercise3/exercise3.java
+++ Chapter47/exercise3/exercise3.java
@@ -1,28 +1,26 @@
-import  java.io.* ;
+import java.io.* ; 
 
 class NearlyZero
 {
 	public static void main ( String[] args )
 	{
-		int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
+		int[] data = {3, 1, 5, 6, 4, 12, -3, 8, -2};
 
 		//declare and initialize variables
-		int closest = 0;
-		int largest = 50;
-		//find the element nearest to zero
-		for(index = 0; index < data.length; index ++ )
+		int largest = 100;
+		int smallest = 0;
+		//find the element neartest to zero
+		for (int index = 0; index < data.length; index ++)
 		{
-			if (Math.abs(0 - data[index]) >= Math.abs(largest - data[index])) {
-				closest = data[index];
+			if (Math.abs(0 - Math.abs(data[index])) <=  Math.abs(largest - Math.abs(data[index]))) {
+				smallest = data[index];
 				largest = data[index];
 			}
 		}
+		// write out the element nearest to zero
+		System.out.println(smallest);
 
-		//write out the element nearest to zero
-		System.out.println(closest);
+
 
 	}
-
-
-
 }
